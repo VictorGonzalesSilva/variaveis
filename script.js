e8c84241ae85a9c37bdb00436d3a6a49c341f347
VictorGonzalesSilva <victorgonzalessilva12@gmail.com>
2021-05-22 11:16:48 -0300

    'variaveis'

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Este é um script para cálculo de idade!
+Declara o ano atual para fazer o cálculo */
+var anoAtual = 2021
+
+// Pede que o usuário digite o ano em que nasceu
+var anoNascimento = prompt ('Digite o ano em que nasceu');
+
+//Calcula a idade do usuário e armazena na viriável
+var idade = anoAtual - anoNascimento
+
+// Mostra ao usuario a idade qeu ele tem
+alert ('Sua idade é:'  + idade + ' anos');
